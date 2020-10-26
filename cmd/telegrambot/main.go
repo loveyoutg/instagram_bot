f06482c50ce56158b61d20212bfe8530a97e60f4
nabil-a-navab <71911759+nabil-a-navab@users.noreply.github.com>
2020-10-26 18:38:47 +0530

    Create main.go

---
+++ cmd/telegrambot/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"Instracker/internal/app/telegrambot"
+	"Instracker/internal/box"
+	"errors"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+
+	configPath, found := os.LookupEnv("CFG_PATH")
+	if !found {
+		print(errors.New("no config path variable"))
+	}
+
+	b, err := box.InitializeBox(configPath)
+	if err != nil {
+		print(err)
+	}
+
+	tgbot, err := telegrambot.NewInstaBot(b)
+	if err != nil {
+		print(err)
+	}
+
+	tgbot.Run()
+}
